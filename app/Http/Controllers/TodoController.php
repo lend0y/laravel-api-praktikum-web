34215d6af9970c7ae9644a9fd02cf931677d9f42
aqayyim <ahmadqoyyim388@gmail.com>
2025-11-24 22:04:52 +0700

    `Added TodoController and apiResource route for todos`

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * GET ALL - menampilkan semua todo (dengan pagination).
+     */
+    public function index()
+    {
+        return Todo::paginate(10);
+    }
+
+    /**
+     * CREATE - Menambahkan todo baru.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required',
+            'description' => 'nullable',
+            'status' => 'nullable',
+            'priority' => 'nullable|integer',
+            'category' => 'nullable'
+        ]);
+
+        return Todo::create($data);
+    }
+
+    /**
+     * GET BY ID - Menampilkan detail todo berdasarkan id.
+     */
+    public function show($id)
+    {
+        return Todo::findOrFail($id);
+    }
+
+    /**
+     * UPDATE - Perbarui data todo berdasarkan id.
+     */
+    public function update(Request $request, $id)
+    {
+        $todo = Todo::findOrFail($id);
+        $todo->update($request->all());
+
+        return $todo;
+    }
+
+    /**
+     * DELETE - Soft delete todo berdasarkan id.
+     */
+    public function destroy($id)
+    {
+        $todo = Todo::findOrFail($id);
+        $todo->delete();
+
+        return response()->json([
+            'message' => 'Deleted successfully'
+        ]);
+    }
+}
---
+++ app/Models/Todo.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Todo extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'priority',
+        'category'
+    ];
+
+    protected $dates = ['deleted_at'];
+}
