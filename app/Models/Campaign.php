ba0ddfd90e5cc9988e137a0b9bef51e170cf1e44
aqayyim <ahmadqoyyim388@gmail.com>
2025-12-02 23:42:45 +0700

    `Added CampaignController to routes/api.php`

---
+++ app/Models/Campaign.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Campaign extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'disaster_type',
+        'location',
+        'target_amount',
+        'collected_amount',
+        'status',
+        'start_date',
+        'end_date',
+    ];
+
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date' => 'date',
+    ];
+}
